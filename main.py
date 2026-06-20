06e6fc413718db815005f0e8dbf2e242e71462c1
Vivaan-coder1 <anubhaasthana13@gmail.com>
2026-06-20 12:23:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Print a sentence
+print("Welcome to the world of programming")
+#Print a Number
+print(4)
+#Next line after print
+print("Hello Python")
+#Pass multiple arguments for printig
+print("hello" ,5)
+#End argument of print statement
+print("welcome to", end= "+")
